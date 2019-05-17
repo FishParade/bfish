6075133023b61db6ba1fbff9e32a9c899dcfd2e2
Brian Fisher <brian.f.fisher@gmail.com>
2019-05-17 09:40:30 -0400

    Updated to handle faceting by numeric/logical variables.

--- R/facetpie.R
+++ R/facetpie.R
@@ -2,17 +2,23 @@
 
 facetpie <- function(df, facetvar, fillvar, pie_title, pie_subtitle, pie_fill) {
   tempdf <- df %>%
-    group_by(!!sym(facetvar), !!sym(fillvar)) %>%
+    mutate(
+      temp_facvar  = factor(!!sym(facetvar)),
+      temp_fillvar = factor(!!sym(fillvar))
+    ) %>%
+    group_by(temp_facvar, temp_fillvar) %>%
     summarize(num_grp = n()) %>%
     ungroup() %>%
-    group_by(!!sym(facetvar)) %>%
+    group_by(temp_facvar) %>%
     mutate(num_facet = sum(num_grp)) %>%
     ungroup() %>%
-    mutate(frac_grp = num_grp / num_facet,
-           facetvar = fct_reorder(!!sym(facetvar), num_facet))
+    mutate(
+      frac_grp = num_grp / num_facet,
+      temp_facvar = fct_reorder(temp_facvar, num_facet, .desc = TRUE)
+      )
 
   tempdf %>%
-    ggplot(aes(x = factor(1), y = frac_grp ,fill = !!sym(fillvar))) +
+    ggplot(aes(x = factor(1), y = frac_grp ,fill = temp_fillvar)) +
     geom_bar(width = 1, stat = "identity") + coord_polar(theta = "y", start = 0) +
     geom_text(aes(x = 1, label = glue::glue("{num_grp} \n ({percent(frac_grp, 2)})"),
                   fontface = "bold"),
@@ -29,6 +35,6 @@ facetpie <- function(df, facetvar, fillvar, pie_title, pie_subtitle, pie_fill) {
       axis.text.y = element_blank()
     ) +
     scale_fill_iu() +
-    facet_wrap(formula(paste("~", facetvar)),
-               labeller = label_wrap_gen(width = 16, multi_line = TRUE))
+    facet_wrap(~temp_facvar, labeller = label_wrap_gen(width = 16, multi_line = TRUE))
 }
+
