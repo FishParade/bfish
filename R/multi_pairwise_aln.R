cf8555653772372b9f0ed77b80903dd6c9db8673
Brian Fisher <brian.f.fisher@gmail.com>
2019-07-29 12:52:01 -0400

    Update multi_pairwise_aln.R
    Summary .csv no longer includes self alignments.

--- R/multi_pairwise_aln.R
+++ R/multi_pairwise_aln.R
@@ -1,7 +1,7 @@
 # Function for a set of pairwise alignments of sequences in two dataframes.
-# Dataframes need solumns named "sequence" and "uniparc_id"
+# Dataframes need columns named "sequence" and "uniparc_id"
 
-multi_pairwise_aln <- function(pattern_df, subject_df, pid_alg = "PID1", sub_matrix = "BLOSUM62") {
+multi_pairwise_aln2 <- function(pattern_df, subject_df, pid_alg = "PID1", sub_matrix = "BLOSUM62") {
 
   # Check if valid substition matrix.
   if (!sub_matrix %in% c("BLOSUM45", "BLOSUM50", "BLOSUM62", "BLOSUM80", "BLOSUM100")) {
@@ -88,6 +88,7 @@ multi_pairwise_aln <- function(pattern_df, subject_df, pid_alg = "PID1", sub_mat
   
   # Summarize the full dataframe by highest %ID alignment for each sequence
   alndf_summary <- full_alndf %>% 
+    filter(pid != 100) %>% 
     group_by(pattern_uniparc) %>% 
     filter(pid == max(pid)) %>% 
     distinct(pattern_uniparc, .keep_all = TRUE)
